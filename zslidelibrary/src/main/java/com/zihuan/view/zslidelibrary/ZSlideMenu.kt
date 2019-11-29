9fc60a30853b2de541c396e91e104e718a67d315
紫幻 <31381762+zihuan1@users.noreply.github.com>
2019-11-29 22:33:57 +0800

    Update ZSlideMenu.kt

--- zslidelibrary/src/main/java/com/zihuan/view/zslidelibrary/ZSlideMenu.kt
+++ zslidelibrary/src/main/java/com/zihuan/view/zslidelibrary/ZSlideMenu.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.LinearLayout
-import com.zihuan.library.R
+import com.zihuan.view.zslidemenu.R
 
 class ZSlideMenu : LinearLayout {
 
